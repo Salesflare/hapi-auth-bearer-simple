6cd3f64d9dbc4c37036b66b52f9ee8cb2d6442c0
AdriVanHoudt <adri.van.houdt@gmail.com>
2014-12-01 20:32:34 +0100

    propper update to hapi 8 // added node 11 to travis

--- test/index.js
+++ test/index.js
@@ -9,51 +9,51 @@ var it = lab.it;
 var expect = Code.expect;
 
 lab.experiment('Integration', function () {
-    it('authenticates a request', function (done) {
-        var server = new Hapi.Server();
-        server.connection();
+   it('authenticates a request', function (done) {
+      var server = new Hapi.Server();
+      server.connection();
 
-        server.register(require('../'), function (err) {
-            expect(err).to.not.exist();
+      server.register(require('../'), function (err) {
+         expect(err).to.not.exist();
 
-            server.auth.strategy('default', 'bearerAuth', true, {
-                validateFunction: function (token, callback) {
-                    var validated_user = {
-                        email: 'test@test.com'
-                    };
-                    return callback(null, token === 'abc', validated_user);
-                }
+         server.auth.strategy('default', 'bearerAuth', true, {
+            validateFunction: function (token, callback) {
+               var validated_user = {
+                  email: 'test@test.com'
+               };
+               return callback(null, token === 'abc', validated_user);
+            }
+         });
+
+
+         server.route({
+            method: 'GET',
+            path: '/login/{user}',
+            config: {
+               auth: 'default',
+               handler: function (request, reply) {
+                  return reply(request.auth.credentials);
+               }
+            }
+         });
+
+         server.inject({
+            method: 'GET',
+            url: '/login/testuser',
+            headers: {
+               Authorization: 'Bearer abc'
+            }
+         }, function (res) {
+            expect(res.statusCode).to.equal(200);
+            expect(res.result).to.deep.equal({
+               email: 'test@test.com',
+               token: 'abc'
             });
 
 
-            server.route({
-                method: 'GET',
-                path: '/login/{user}',
-                config: {
-                    auth: 'default',
-                    handler: function (request, reply) {
-                        return reply(request.auth.credentials);
-                    }
-                }
-            });
+            done();
+         });
 
-            server.inject({
-                method: 'GET',
-                url: '/login/testuser',
-                headers: {
-                    Authorization: 'Bearer abc'
-                }
-            }, function (res) {
-                expect(res.statusCode).to.equal(200);
-                expect(res.result.credentials).to.deep.equal({
-                    email: 'test@test.com',
-                    token: 'abc'
-                });
-
-
-                done();
-            });
-
-        });
-    });
+      });
+   });
 });
